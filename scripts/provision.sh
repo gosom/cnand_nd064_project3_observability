4e7710c83dde096c621fd5901a848b256b01850b
Giorgos Komninos <giorgos@tabeo.co.uk>
2021-11-11 07:32:09 +0200

    adds basic dashboard

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,8 +4,7 @@ set -e
 echo "**** Begin installing k3s"
 
 #Install
-#curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" sh -
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" sh -
 echo "**** End installing k3s"
 
 #kubectl proxy --address='0.0.0.0' /dev/null &
